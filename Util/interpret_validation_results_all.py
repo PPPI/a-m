20191ceb15e873e45388cd4c312a1f30834dfd62
Profir-Petru Partachi <profir.p.partachi@gmail.com>
2018-05-07 17:27:53 +0100

    Fix interpretation script to correctly count positive and negative events from predictions

--- Util/interpret_validation_results_all.py
+++ Util/interpret_validation_results_all.py
@@ -8,19 +8,20 @@ import numpy as np
 import pandas as pd
 
 
-def evaluate_at_threshold(result_p, result_i, th, top_k, truth_p, truth_i):
+def evaluate_at_threshold(result_p, result_i, th, top_k, truth_p, truth_i, subseq_p, subseq_i):
     if top_k == 'inf':
         result_p_ = [(pr, sorted([(issue, prob) for issue, prob in pred if prob >= th],
-                               reverse=True, key=lambda p: (p[1], p[0]))) for pr, pred in result_p.items()]
+                                 reverse=True, key=lambda p: (p[1], p[0]))) for pr, pred in result_p.items()]
     else:
         result_p_ = [(pr, sorted([(issue, prob) for issue, prob in pred if prob >= th],
-                               reverse=True, key=lambda p: (p[1], p[0]))[:top_k]) for pr, pred in result_p.items()]
+                                 reverse=True, key=lambda p: (p[1], p[0]))[:top_k]) for pr, pred in result_p.items()]
     if top_k == 'inf':
         result_i_ = [(issue, sorted([(pr, prob) for pr, prob in pred if prob >= th],
-                               reverse=True, key=lambda p: (p[1], p[0]))) for issue, pred in result_i.items()]
+                                    reverse=True, key=lambda p: (p[1], p[0]))) for issue, pred in result_i.items()]
     else:
         result_i_ = [(issue, sorted([(pr, prob) for pr, prob in pred if prob >= th],
-                               reverse=True, key=lambda p: (p[1], p[0]))[:top_k]) for issue, pred in result_i.items()]
+                                    reverse=True, key=lambda p: (p[1], p[0]))[:top_k]) for issue, pred in
+                     result_i.items()]
     hit = 0
     ap = 0
     mrr = .0
@@ -28,6 +29,9 @@ def evaluate_at_threshold(result_p, result_i, th, top_k, truth_p, truth_i):
     fpr = 0
     fnr = 0
     renorm = 0
+    neg_renorm = 0
+    sub_hit = 0
+    sub_total = 0
 
     for pr_nr, predictions in result_p_:
         predictions = [p[0] for p in predictions]
@@ -39,10 +43,22 @@ def evaluate_at_threshold(result_p, result_i, th, top_k, truth_p, truth_i):
         except KeyError:
             expected_truth = set()
 
+        try:
+            subseq_link = subseq_p['#' + pr_nr[len('issue_'):]]
+        except KeyError:
+            subseq_link = list()
+
+        subseqent_predictions = [('#' + val[len('issue_'):] in subseq_link) for val in predictions]
+        subseqent_predictions = [val for val in subseqent_predictions if val is True]
+
+        sub_hit += len(subseqent_predictions)
+        sub_total += len(subseq_link)
+
         predictions = [('#' + val[len('issue_'):] in expected_truth) for val in predictions]
 
         if (len(expected_truth) > 0) and (len(predictions) > 0):
             renorm += 1
+            neg_renorm += 1
             hit += any(predictions)
             curr_ap = np.mean([len([val for val in predictions[:(i + 1)] if val is True]) / (i + 1)
                                for i, j in enumerate(predictions) if j is True])
@@ -55,9 +71,10 @@ def evaluate_at_threshold(result_p, result_i, th, top_k, truth_p, truth_i):
             dcg += sum([1 / (math.log(i + 1, 2) if i > 1 else 1) if j is True else 0
                         for i, j in enumerate(predictions)])
         elif (len(expected_truth) == 0) and (len(predictions) > 0):
-            # renorm += 1
+            renorm += 1
             fpr += 1
         elif (len(expected_truth) > 0) and (len(predictions) == 0):
+            neg_renorm += 1
             fnr += 1
         elif (len(expected_truth) == 0) and (len(predictions) == 0):
             hit += 1
@@ -72,10 +89,22 @@ def evaluate_at_threshold(result_p, result_i, th, top_k, truth_p, truth_i):
         except KeyError:
             expected_truth = set()
 
+        try:
+            subseq_link = subseq_i['#' + issue[len('issue_'):]]
+        except KeyError:
+            subseq_link = list()
+
+        subseqent_predictions = [('#' + val[len('issue_'):] in subseq_link) for val in predictions]
+        subseqent_predictions = [val for val in subseqent_predictions if val is True]
+
+        sub_hit += len(subseqent_predictions)
+        sub_total += len(subseq_link)
+
         predictions = [('#' + val[len('issue_'):] in expected_truth) for val in predictions]
 
         if (len(expected_truth) > 0) and (len(predictions) > 0):
             renorm += 1
+            neg_renorm += 1
             hit += any(predictions)
             curr_ap = np.mean([len([val for val in predictions[:(i + 1)] if val is True]) / (i + 1)
                                for i, j in enumerate(predictions) if j is True])
@@ -88,9 +117,10 @@ def evaluate_at_threshold(result_p, result_i, th, top_k, truth_p, truth_i):
             dcg += sum([1 / (math.log(i + 1, 2) if i > 1 else 1) if j is True else 0
                         for i, j in enumerate(predictions)])
         elif (len(expected_truth) == 0) and (len(predictions) > 0):
-            # renorm += 1
+            renorm += 1
             fpr += 1
         elif (len(expected_truth) > 0) and (len(predictions) == 0):
+            neg_renorm += 1
             fnr += 1
         elif (len(expected_truth) == 0) and (len(predictions) == 0):
             hit += 1
@@ -99,12 +129,13 @@ def evaluate_at_threshold(result_p, result_i, th, top_k, truth_p, truth_i):
     mrr = mrr / renorm if renorm > 0 else mrr
     ap = ap / renorm if renorm > 0 else ap
     dcg = dcg / renorm if renorm > 0 else dcg
+    fpr = fpr / renorm if renorm > 0 else fpr
+    fnr = fnr / neg_renorm if neg_renorm > 0 else fnr
     # hit = hit / renorm if renorm > 0 else hit
     hit /= (len(result_i_) + len(result_p_))
-    fpr /= (len(result_i_) + len(result_p_))
-    fnr /= (len(result_i_) + len(result_p_))
+    sub_hit = sub_hit / sub_total if sub_total > 0 else sub_hit
 
-    return hit, ap, mrr, dcg, fpr, fnr
+    return hit, ap, mrr, dcg, fpr, fnr, sub_hit
 
 
 if __name__ == '__main__':
@@ -117,7 +148,8 @@ if __name__ == '__main__':
         try:
             results_i = list()
             for fold in [4]:
-                with open((location_format[:-5] + '_RAW_i_results_f4_ioTrue_poTrue_tTrue_csTrue_jFalse_fFalse_sFalse_u1.txt') % project) as f:
+                with open((location_format[
+                           :-5] + '_RAW_i_results_f4_ioTrue_poTrue_tTrue_csTrue_jFalse_fFalse_sFalse_u1.txt') % project) as f:
                     result_str = f.read()
                 result = ast.literal_eval(result_str)
                 results_i.append((fold, result))
@@ -125,7 +157,8 @@ if __name__ == '__main__':
 
             results_p = list()
             for fold in [4]:
-                with open((location_format[:-5] + '_RAW_p_results_f4_ioTrue_poTrue_tTrue_csTrue_jFalse_fFalse_sFalse_u1.txt') % project) as f:
+                with open((location_format[
+                           :-5] + '_RAW_p_results_f4_ioTrue_poTrue_tTrue_csTrue_jFalse_fFalse_sFalse_u1.txt') % project) as f:
                     result_str = f.read()
                 result = ast.literal_eval(result_str)
                 results_p.append((fold, result))
@@ -143,15 +176,32 @@ if __name__ == '__main__':
                     except KeyError:
                         truth_pr[pr] = {issue}
 
+            with open(os.path.join(os.path.dirname(location_format[:-5]), '..', 'tails',
+                                   os.path.basename(location_format)[:-len('.json')] + '_subsequent.json') % project) \
+                    as f:
+                subseq_p = jsonpickle.decode(f.read())
+
+            subseq_i = dict()
+            for pr in subseq_p.keys():
+                for issue in subseq_p[pr]:
+                    try:
+                        subseq_i[issue].add(pr)
+                    except KeyError:
+                        subseq_i[issue] = {pr}
+
             data = {'Threshold': list(), 'K': list(), 'Hit-rate': list(), 'Average Precision': list(),
                     'Mean Reciprocal Rank': list(), 'Discounted Cumulative Gain': list(),
-                    'False Positive Rate': list(), 'False Negative Rate': list()}
+                    'False Positive Rate': list(), 'False Negative Rate': list(),
+                    'Subsequent Links Found': list()}
             for fold in [4]:
                 result_p = results_p[fold]
                 result_i = results_i[fold]
                 for th in np.arange(.0, 1., step=.01):
                     for top_k in [1, 3, 5, 7, 10, 15, 20, 'inf']:
-                        hit, ap, mrr, dcg, fpr, fnr = evaluate_at_threshold(result_p, result_i, th, top_k, truth_pr, truth)
+                        hit, ap, mrr, dcg, fpr, fnr, sub_hit = evaluate_at_threshold(result_p, result_i, th, top_k,
+                                                                                     truth_pr, truth,
+                                                                                     subseq_i=subseq_i,
+                                                                                     subseq_p=subseq_p)
 
                         data['Threshold'].append(float('%.5f' % th))
                         data['K'].append(top_k)
@@ -161,7 +211,10 @@ if __name__ == '__main__':
                         data['Discounted Cumulative Gain'].append(dcg)
                         data['False Positive Rate'].append(fpr)
                         data['False Negative Rate'].append(fnr)
+                        data['Subsequent Links Found'].append(sub_hit)
 
-            pd.DataFrame(data=data).to_csv((location_format[:-len('.json')] + '_results_interpreted_MF_restricted.csv') % project)
+            pd.DataFrame(data=data).to_csv(
+                (location_format[:-len('.json')] + '_results_interpreted_MF_restricted.csv') % project)
         except Exception as e:
-            pass
+            print('%s' % project)
+            print(str(e))
