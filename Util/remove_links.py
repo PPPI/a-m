73b318fd994d8b0403f5273fbb92634af019a628
Profir-Petru Partachi <anon@nymo.us>
2019-06-11 14:12:29 +0100

    Generate all files at different keep rates

--- Util/remove_links.py
+++ Util/remove_links.py
@@ -2,9 +2,10 @@ import random
 import sys
 import jsonpickle
 
+import numpy as np
+
 if __name__ == '__main__':
     location = sys.argv[1]
-    keep_rate = float(sys.argv[2])
 
     with open(location[:-5] + '_truth.json') as f:
         truth = jsonpickle.decode(f.read())
@@ -14,16 +15,18 @@ if __name__ == '__main__':
     pr_nr = [pr.number for pr in repo.prs]
     issue_ids = [i.id_ for i in repo.issues]
 
-    noisy_truth = dict()
-    for issue_id, pr_nrs in truth.items():
-        for pr_nr in pr_nrs:
-            roll = random.uniform(0.0, 1.0)
-            target = keep_rate
-            if roll > target:
-                try:
-                    noisy_truth[issue_id].append(pr_nr)
-                except KeyError:
-                    noisy_truth[issue_id] = [pr_nr]
+    for keep_rate in np.arange(0.05, 0.95, 0.05):
+        keep_rate = float('%2.3f' % keep_rate)
+        noisy_truth = dict()
+        for issue_id, pr_nrs in truth.items():
+            for pr_nr in pr_nrs:
+                roll = random.uniform(0.0, 1.0)
+                target = keep_rate
+                if roll > target:
+                    try:
+                        noisy_truth[issue_id].append(pr_nr)
+                    except KeyError:
+                        noisy_truth[issue_id] = [pr_nr]
 
     with open(location[:-5] + '_r_%2.2f_truth.json' % keep_rate, 'w') as f:
         f.write(jsonpickle.encode(noisy_truth))
